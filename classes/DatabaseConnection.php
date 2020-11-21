c9b52a39dd0b900e0577bc381947f8ae71a72312
david-kariuki <dkaris.k@gmail.com>
2020-11-21 05:44:45 +0300

    Renamed Connection.php to DatabaseConnection.php

---
+++ classes/DatabaseConnection.php
@@ -0,0 +1,45 @@
+<?php
+
+//    Copyright (c) 2020 by David Kariuki (dk).
+//    All Rights Reserved.
+
+
+class DatabaseConnection
+{
+
+    // Connection status value variable
+    private $connectToDB;
+
+
+    /**
+    * Class destructor
+    */
+    function __construct()
+    {}
+
+
+    /**
+    * Class destructor
+    */
+    function __destruct()
+    {
+		mysqli_close($this->connectToDB); // Close database connection
+    }
+
+
+    // Connecting to database
+    public function Connect()
+    {
+
+        // Call configuration file
+        require_once 'configs/DatabaseConfiguration.php';
+
+        // Connecting to MYSQL database
+        $this->connectToDB = new mysqli(HOST, HOST_USERNAME, HOST_PASSWORD, DATABASE_NAME);
+
+        // Return database handler
+        return $this->connectToDB;
+    }
+}
+
+?>
