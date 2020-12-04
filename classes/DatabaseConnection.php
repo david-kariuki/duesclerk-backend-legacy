0c76bd72b2b5d406994618b8acca029d305a428d
david-kariuki <dkaris.k@gmail.com>
2020-12-04 21:59:52 +0300

    Implemented singleton pattern in database connection class.

--- classes/DatabaseConnection.php
+++ classes/DatabaseConnection.php
@@ -4,40 +4,95 @@
 //    All Rights Reserved.
 
 
+/**
+* Mysql database class - only one connection alowed
+*/
 class DatabaseConnection
 {
 
     // Connection status value variable
-    private $connectToDB;
+    private $databaseConnection;
+
+    // Databse single connection instance
+    private static $connectionInstance;
 
 
     /**
-    * Class destructor
+    * Class constructor
     */
-    function __construct()
-    {}
+    private function __construct()
+    {
+        // Call configuration file
+        require_once 'configs/DatabaseConfiguration.php';
+
+        // Connecting to MYSQL database
+        $this->databaseConnection = new mysqli(
+            HOST,
+            HOST_USERNAME,
+            HOST_PASSWORD,
+            DATABASE_NAME
+        );
+
+        // Check if connection was successful
+        if ($this->databaseConnection->connect_error) {
+            // Connection error
+
+            trigger_error(
+                "Connection Error: " . $this->databaseConnection->connect_error(),
+                E_USER_ERROR
+            );
+        }
+
+    }
 
 
     /**
     * Class destructor
     */
     function __destruct()
-    {}
-
-
-    // Connecting to database
-    public function Connect()
     {
 
-        // Call configuration file
-        require_once 'configs/DatabaseConfiguration.php';
-
-        // Connecting to MYSQL database
-        $this->connectToDB = new mysqli(HOST, HOST_USERNAME, HOST_PASSWORD, DATABASE_NAME);
-
-        // Return database handler
-        return $this->connectToDB;
     }
+
+
+    /**
+    * Get an instance of the database connection
+    *
+    * @return self instance - connection instance
+    */
+    public static function getConnectionInstance()
+    {
+        // Check for another existent instance
+        if (!self::$connectionInstance) {
+
+            // Create new instance if not existing
+            self::$connectionInstance = new self();
+        }
+
+        return self::$connectionInstance; // Return instance
+    }
+
+
+    /**
+    * Clone() method
+    * Leaving it empty to prevent duplication of database connection
+    */
+    private function __clone()
+    {
+
+    }
+
+
+    /**
+    * Function to get database connection
+    *
+    * @return mysqli - databaseConnection
+    */
+    public function getDatabaseConnection()
+    {
+        return $this->databaseConnection; // Return database connection
+    }
+
 }
 
-?>
+// EOF: DatabaseConnection.php
