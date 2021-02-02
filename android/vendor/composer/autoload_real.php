62fee56c67eebc37ef38718aa661b8d13fee6e3e
david-kariuki <dkaris.k@gmail.com>
2021-02-02 20:56:46 +0300

    Compose update and file renaming.

--- android/vendor/composer/autoload_real.php
+++ android/vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInit9e857291db0757375cb88789b76cb712
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit9e857291db0757375cb88789b76cb712', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit9e857291db0757375cb88789b76cb712', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
